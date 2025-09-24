2df2f951e1f76c72faa3861d930824aeef1df09a
felix634 <felixbary@gmail.com>
2025-09-24 20:35:30 +0200

    removed incompatible image integration and updated image tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import image from '@astrojs/image'; // Import the image integration
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()]
+  integrations: [tailwind()]
 });
